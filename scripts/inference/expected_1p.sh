ce9558aba74b87f884d5360572e447bebccf7cb4
Yu Mo <moyu@login1.hsn0.cm.bigred200.uits.iu.edu>
2024-08-27 13:43:48 -0400

    slurm script

--- scripts/inference/expected_1p.sh
+++ scripts/inference/expected_1p.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
-#SBATCH -J expectandsp_1p
+#SBATCH -J 6taxa_cond1_gt_1p
 #SBATCH -p general
-#SBATCH -o high_2_1p.txt
-#SBATCH -e high_2_1p.err
+#SBATCH -o 6taxa_cond1_gt_1p_%j.txt
+#SBATCH -e 6taxa_cond1_gt_1p_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=moyu@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=7:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=1G
-#SBATCH --cpus-per-task=4
 #SBATCH -A r00526
 
-export OMP_NUM_THREADS=4
+#Load any modules that your program needs
+source ~/spinney/spinney_env/bin/activate
 
-condition=high_2;
+for condition in "6taxa_condition1";
+do
 
-traitfile=6taxa_${condition}.csv;
-gtree=6taxa_${condition}.expected.txt;
-sptree=6taxa_${condition}.sptree.nwk;
-cdtree=6taxa_${condition}.concord.nwk;
-numtree=4;
+cd ~/spinney/experiment/$condition
+traitfile=${condition}.csv;
+gtree=${condition}.sampled.txt;
+sptree=${condition}.sptree.txt;
+cdtree=${condition}.concord.txt;
+numtree=10;
 
-cd ~/spinney
-module load python/3.9
+echo "" > ${condition}.tsv;
+pypath=../../code/sampledtree_rates.py
 
 ### gene trees
 #1-param
-python ./code/sampledtree_rates.py -f ./trait/$traitfile -N $numtree -genetrees ./tree/$gtree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_1p.ratetree.nwk -condition ${condition}_expected_1p
-
+python $pypath -f ./$traitfile -N $numtree -genetrees ./$gtree -speciestree ./$sptree -ratetree ../6taxa_1p.ratetree.nwk -condition ${condition}_sampled_1p
 #2-param
-#python ./code/sampledtree_rates.py -f ./trait/$traitfile -N $numtree -genetrees ./tree/$gtree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_2p.ratetree.nwk -condition ${condition}_expected_2p
+#python $pypath -f ./$traitfile -N $numtree -genetrees ./$gtree -speciestree ./$sptree -ratetree ../6taxa_2p.ratetree.nwk -condition ${condition}_sampled_2p
 
-### species tree
-python ./code/sampledtree_rates.py -f ./trait/$traitfile -N 1 -genetrees ./tree/$sptree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_1p.ratetree.nwk  -condition ${condition}_sp_1p
 
-#python ./code/sampledtree_rates.py -f ./trait/$traitfile -N 1 -genetrees ./tree/$sptree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_2p.ratetree.nwk  -condition ${condition}_sp_2p
-
-### concordant tree
-#python ./code/sampledtree_rates.py -f ./trait/$traitfile -N 1 -genetrees ./tree/$cdtree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_1p.ratetree.nwk -condition ${condition}_concord_1p
-
-#python ./code/sampledtree_rates.py -f ./trait/$traitfile -N 1 -genetrees ./tree/$cdtree -speciestree ./tree/$sptree -ratetree ./tree/6taxa_2p.ratetree.nwk -condition ${condition}_concord_2p
+done
